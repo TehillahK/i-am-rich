21010757bd2e7a9d82ef5cb0ad26fb5eb7e9455b
Tehillahk <kangambt@myumanitoba.ca>
2025-01-10 17:56:45 -0600

    Initial Commit

---
+++ i-am-rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i-am-rich
+//
+//  Created by Tehillah Kangamba on 2025-01-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
